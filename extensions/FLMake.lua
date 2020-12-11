e5fd93470c99cd034abdf59c9b44e606cb2e655a
T 'Filtered' C <41757236+FilteredDev@users.noreply.github.com>
2020-12-11 22:06:10 +0000

    Update FLMake.lua

--- extensions/FLMake.lua
+++ extensions/FLMake.lua
@@ -4,6 +4,15 @@ local flmk = {}
 flmk.ID = "FLMake"
 
 function flmk:start(fl)
+	function self:GetAllExtensions()
+		local r = {}
+		for _, v in ipairs(script.Parent:GetChildren()) do
+			local id = require(v).ID --we have to fetch the ID, so we'll load the module.
+			r[id] = fl:WaitForExtension(id)
+		end
+		return r
+	end
+	
 	function self:Compile(lmk)
 		local rd = {}
 		local intendedLibrariesToLoad = #lmk
